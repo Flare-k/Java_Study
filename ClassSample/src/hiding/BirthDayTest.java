df6d7a97990c3c9e07806fe6f3a1f3d20cfd47b1
Flare-k <rokkyw@naver.com>
2020-12-21 21:01:34 +0900

    Class Hiding, Using 'private instance' -> public method

--- ClassSample/src/hiding/BirthDayTest.java
+++ ClassSample/src/hiding/BirthDayTest.java
@@ -4,15 +4,40 @@ class BirthDay {
     private int day;
     private int month;
     private int year;
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
 }
 
 public class BirthDayTest{
     public static void main(String[] args){
         BirthDay day = new BirthDay();
 
-        day.month = 5;
-        day.year = 2020;
-        day.day = 29;
+        day.setMonth(5);
+        day.setYear(2020);
+        day.setDay(29);
+        System.out.println(day.getYear() + "년 " + day.getMonth() + "월 " + day.getDay() + "일");
     }
 }
 // 하나의 파일안에 클래스가 여러 개일수도 있다.
